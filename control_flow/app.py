a0dd51a9aec6bde1e611e8bf9e137d2535003d23
bavinclint <bavinclint@gmail.com>
2023-07-20 11:33:13 +0300

    Fibonacci series

--- control_flow/app.py
+++ control_flow/app.py
@@ -3,9 +3,17 @@
 # message = "Eligible" if age >= 18 else "Not eligible"
 # print(message)
 
-count = 0
-for number in range(1, 10):
-    if number % 2 == 0:
-        count += 1
-        print(number)
-print(f"We have {count} even numbers")
+# iterate even numbers
+# count = 0
+# for number in range(1, 10):
+#     if number % 2 == 0:
+#         count += 1
+#         print(number)
+# print(f"We have {count} even numbers")
+
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
