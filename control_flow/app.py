6d9d49eb89f095ceb772cf3d045cdab08223e4e6
bavinclint <bavinclint@gmail.com>
2023-07-20 09:20:50 +0300

    iterate even numbers

--- control_flow/app.py
+++ control_flow/app.py
@@ -1,3 +1,11 @@
-age = 22
-message = "Eligible" if age >= 18 else "Not eligible"
-print(message)
+# tenary operator
+# age = 22
+# message = "Eligible" if age >= 18 else "Not eligible"
+# print(message)
+
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
