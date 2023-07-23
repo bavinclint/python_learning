97a05ae641c890266773eca773f61a80baae2e18
bavinclint <bavinclint@gmail.com>
2023-07-23 20:30:29 +0300

    prime number

--- control_flow/task.py
+++ control_flow/task.py
@@ -29,3 +29,11 @@
 #     if a == "q" or a == "e":
 #         continue
 #     print("{}".format(a), end='')
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
