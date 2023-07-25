22a7ebe1060267b168f31a8abf3faa58ecd1ca2e
bavinclint <bavinclint@gmail.com>
2023-07-25 14:44:17 +0300

    properties

--- control_flow/class_lec.py
+++ control_flow/class_lec.py
@@ -14,30 +14,49 @@
 # point = Point.zero()
 # print(point.draw())
 
-class TagCloud:
-    def __init__(self):
-        self.__tags = {}
+# class TagCloud:
+#     def __init__(self):
+#         self.__tags = {}
 
-    def add(self, tag):
-        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+#     def add(self, tag):
+#         self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
 
-    def __getitem__(self, tag):
-        return self.__tags.get(tag.lower(), 0)
+#     def __getitem__(self, tag):
+#         return self.__tags.get(tag.lower(), 0)
 
-    def __setitem__(self, tag, count):
-        self.__tags[tag.lower()] = count
+#     def __setitem__(self, tag, count):
+#         self.__tags[tag.lower()] = count
 
-    def __len__(self):
-        return len(self.__tags)
+#     def __len__(self):
+#         return len(self.__tags)
 
-    def __iter__(self):
-        return iter(self.__tags)
+#     def __iter__(self):
+#         return iter(self.__tags)
 
 
-cloud = TagCloud()
-cloud["python"] = 10
-len(cloud)
-cloud.add("Python")
-cloud.add("python")
-cloud.add("python")
-print(cloud.__tags["PYTHON"])
+# cloud = TagCloud()
+# # cloud["python"] = 10
+# # len(cloud)
+# # cloud.add("Python")
+# # cloud.add("python")
+# # cloud.add("python")
+# # print(cloud.__tags["PYTHON"])
+# print(cloud._TagCloud__tags)
+
+class Product:
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative.")
+        self.__price = value
+
+
+product = Product(10)
+print(product.price)
