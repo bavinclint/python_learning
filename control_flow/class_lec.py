1538fcf6128a773dcd481c950985debfb90a22c0
bavinclint <bavinclint@gmail.com>
2023-07-25 14:18:28 +0300

    custom containers

--- control_flow/class_lec.py
+++ control_flow/class_lec.py
@@ -1,11 +1,43 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
+#     @classmethod
+#     def zero(cls):
+#         return cls(0, 0)
+
+#     def draw(self):
+#         print(f"Point ({self.x}, {self.y})")
 
 
-point = Point(1, 2)
-print(point.draw())
+# point = Point.zero()
+# print(point.draw())
+
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+cloud = TagCloud()
+cloud["python"] = 10
+len(cloud)
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
