e98221e71e83e2b9080d5e694c0b87c3cff7a79c
bavinclint <bavinclint@gmail.com>
2023-07-26 17:00:20 +0300

    inhertance

--- control_flow/app2.py
+++ control_flow/app2.py
@@ -1,3 +1,8 @@
 # determine the character that has the most appearance in the string
-sentence = "This is a common interview question"
-sentence
+# sentence = "This is a common interview question"
+# sentence
+
+import requests
+
+response = requests.get("https://google.com")
+print(response)
